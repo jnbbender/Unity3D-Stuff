be1347a4898671d0be64f6ff2fe3c8ffda401d05
jnbbender <58187872+jnbbender@users.noreply.github.com>
2021-10-27 18:40:15 -0400

    Add files via upload

---
+++ Emerald AI/EmeraldAIColliders.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class EmeraldAIColliders : MonoBehaviour
+{
+    [Tag]
+    public string collidersOnTag;
+    public CapsuleCollider[] colliders;
+
+    private bool _enabled;
+    private bool _isTrigger;
+
+    public bool enabled
+    {
+        get { return _enabled; }
+        set { _enabled = value; SetEnabled(); }
+    }
+
+    public bool isTrigger
+    {
+        get { return _isTrigger; }
+        set { _isTrigger = value; SetTrigger(); }
+    }
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (colliders.Length < 1)
+        {
+            CapsuleCollider[] colds = GetComponentsInChildren<CapsuleCollider>();
+            List<CapsuleCollider> bodyParts = new List<CapsuleCollider>();
+
+            for (int i =0; i < colds.Length; ++i)
+            {
+                if (colds[i].CompareTag(collidersOnTag))
+                {
+                    bodyParts.Add(colds[i]);
+                }
+            }
+
+            colliders = bodyParts.ToArray();
+        }
+    }
+
+    void SetEnabled()
+    {
+        foreach (CapsuleCollider collider in colliders)
+            collider.enabled = _enabled;
+    }
+
+    void SetTrigger()
+    {
+        foreach (CapsuleCollider collider in colliders)
+            collider.isTrigger = _isTrigger;
+    }
+}
